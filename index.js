4215a61b907b08237d6c73be0c5c7c43b85100f2
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-01-14 15:44:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let randomNumber1 = Math.floor(Math.random()*6)+1;
 let randomDiceImage = "dice"+randomNumber1+".png";
-let randomImageSource = "images/" + randomDiceImage;
+let randomImageSource = randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 let randomNumber2 = Math.floor(Math.random()*6)+1;
-let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+let randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
